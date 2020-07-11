fc92e5d4eba5bcedd274b3ef4eeecd1f042ff177
Daanjels <admin@daanjels.be>
2020-07-11 15:41:47 +0200

    put default user and password in config

--- src/config/config.php
+++ src/config/config.php
@@ -5,8 +5,8 @@ define('URLROOT', 'http://localhost/structure'); // adapt as needed
 define('SITENAME', 'MVC structure'); // adapt as needed
 // DB params
 define('DB_HOST', 'localhost');
-define('DB_USER', 'phpuser'); // adapt as needed
-define('DB_PASS', 'AzErTy&2"4('); // adapt as needed
+define('DB_USER', 'sqluser'); // adapt as needed
+define('DB_PASS', 'password'); // adapt as needed
 define('DB_NAME', 'mydatabase'); // adapt as needed
 
 // define app root as a constant
